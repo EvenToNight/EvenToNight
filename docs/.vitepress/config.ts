a753a3d0d2f6f467c3602ff4866e28924530883a
Tommaso <tommaso.brini@studio.unibo.it>
2025-08-29 00:01:38 +0200

    chore(init): add gradle structure, vitepress docs and github pages config

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,22 @@
+export default {
+    title: "Report Progetto",
+    themeConfig: {
+        nav: [
+        { text: "Introduzione", link: "/" },
+        { text: "Requisiti", link: "/requisiti" },
+        { text: "Architettura", link: "/architettura" },
+        { text: "Conclusioni", link: "/conclusioni" }
+        ],
+        sidebar: [
+        {
+            text: "Report",
+            items: [
+                { text: "Introduzione", link: "/" },
+                { text: "Requisiti", link: "/requisiti" },
+                { text: "Architettura", link: "/architettura" },
+                { text: "Conclusioni", link: "/conclusioni" }
+              ]
+        }
+        ]
+    }
+}
