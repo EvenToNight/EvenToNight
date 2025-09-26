b508e82ac7c77744a9f57634a5892e84f181b750
Fede802 <federico.bravetti2@studio.unibo.it>
2025-09-26 18:07:02 +0200

    feat(dev-testing): run scala test waiting for dev environment to be available

--- services/users/src/main/scala/Main.scala
+++ services/users/src/main/scala/Main.scala
@@ -1,7 +1,24 @@
+import com.rabbitmq.client.{ConnectionFactory, Connection, Channel}
+
 object Main extends cask.MainRoutes{
+  override def port: Int = 9000 // Use your desired port here
+
+  // --- RabbitMQ connection ---
+  val factory = new ConnectionFactory()
+  factory.setHost("localhost")
+  factory.setPort(5672) // default RabbitMQ port
+  val connection: Connection = factory.newConnection()
+  val channel: Channel = connection.createChannel()
+
+  val queueName = "testQueue"
+  channel.queueDeclare(queueName, false, false, false, null)
+  val message = "Hello from Scala 3!"
+    
   @cask.get("/")
   def hello(request: cask.Request) = {
     print(request.headers)
+    channel.basicPublish("", queueName, null, message.getBytes())
+    println(s"Sent message to RabbitMQ: '$message'")
     "Hello World!"
     "ahgsha"
   }
