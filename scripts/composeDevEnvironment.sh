7d7a556607ce3c9940bd030a0f04e0c6a54a9d1f
Fede802 <federico.bravetti2@studio.unibo.it>
2025-09-29 17:30:08 +0200

    feat(compose-scripts): improve and document scripts to handle compose files

---
+++ scripts/composeDevEnvironment.sh
@@ -0,0 +1,35 @@
+: '
+Developer Environment Runner Script
+
+SYNOPSIS
+    ./composeDevEnvironment.sh [OPTIONS] [DOCKER_COMPOSE_COMMANDS...]
+
+DESCRIPTION
+    This script executes docker compose command
+    by recursively searching for docker-compose.yaml files in ./dev-environment folder.
+    Users can optionally include docker-compose-dev.yaml files in the search.
+    The script uses a .env file situated in the project root for environment variable configuration.
+OPTIONS
+    --dev
+        Include docker-compose-dev.yaml files in the search.
+
+    Other arguments provided will be passed directly to the docker compose command.
+
+EXAMPLES:
+    ./composeApplication.sh up
+        Runs the application.
+
+    ./composeApplication.sh --dev up
+        Runs the application with dev configuration.
+
+NOTES:
+  - Requires Bash and Docker installed.
+  - Changes directory to the project root before execution.
+  - The docker compose project name is set to eventonight-dev-environment.
+'
+#!/bin/bash
+set -e
+
+cd "$(dirname "$0")/.." || exit 1
+
+./scripts/composeAll.sh --project-name eventonight-dev-environment -p ./dev-environment "$@"
