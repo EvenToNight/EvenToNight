8b39c358d75aaaf3c22b1de13f8556cc7ebc2df3
Fede802 <federico.bravetti2@studio.unibo.it>
2025-10-20 19:42:37 +0200

    chore(users): configure rabbit connection to be used in production

--- services/users/build.gradle.kts
+++ services/users/build.gradle.kts
@@ -27,6 +27,8 @@ dependencies {
     testRuntimeOnly("org.junit.platform:junit-platform-engine:1.13.1")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.13.1")
     testRuntimeOnly("org.scalatestplus:junit-5-13_3:3.2.19.0")
+    implementation("io.undertow:undertow-core:2.3.12.Final")
+    implementation("org.jboss.logging:jboss-logging:3.5.3.Final")
 }
 
 
