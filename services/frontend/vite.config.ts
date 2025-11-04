2aac0f2ad7cdb100ac9b24b8c5dadd133670f4b5
Fede802 <federico.bravetti2@studio.unibo.it>
2025-11-04 16:52:06 +0100

    chore(vite-config): update syntax to fix dev-tools optional import

--- services/frontend/vite.config.ts
+++ services/frontend/vite.config.ts
@@ -1,14 +1,14 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import VueDevTools from 'vite-plugin-vue-devtools'
 
 const isDev = process.env.NODE_ENV !== 'production'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), ...(isDev ? [require('vite-plugin-vue-devtools').default()] : [])],
+  plugins: [vue(), vueJsx(), ...(isDev ? [VueDevTools()] : [])],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
