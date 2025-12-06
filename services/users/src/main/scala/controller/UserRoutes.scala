b97bafe54ccb574e92a4e2dfeb43f454d1d332e7
Alice Alfonsi <alice.alfonsi2@studio.unibo.it>
2025-12-06 10:39:46 +0100

    chore(UserRoutes): remove temporary /users test endpoint

--- services/users/src/main/scala/controller/UserRoutes.scala
+++ services/users/src/main/scala/controller/UserRoutes.scala
@@ -13,26 +13,5 @@ class UserRoutes(userService: UserService) extends cask.Routes {
     "Hello World!"
   }
 
-  @cask.get("/users")
-  def createUser(): String = {
-    val memberId: String        = "b07bfb2c-45f8-4f51-b6a8-c29d7895f53d"
-    val memberEmail: String     = "bobsmith@test.com"
-    val memberAccountId: String = memberId
-    val memberNickname: String  = "bobo"
-    import model.*
-    import model.member.*
-    val member: Member = Member(MemberAccount(memberId, memberEmail), MemberProfile(memberAccountId, memberNickname))
-    userService.insertUser(member)
-    val orgId: String        = "7e63d360-5e3d-4b33-8c9a-a8c2d773db2c"
-    val orgEmail: String     = "info@meet.com"
-    val orgAccountId: String = orgId
-    val orgNickname: String  = "Meet"
-    import model.organization.*
-    val organization: Organization =
-      Organization(OrganizationAccount(orgId, orgEmail), OrganizationProfile(orgAccountId, orgNickname))
-    userService.insertUser(organization)
-    "Users inserted"
-  }
-
   initialize()
 }
