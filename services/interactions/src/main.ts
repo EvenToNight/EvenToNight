40c1f44ca92e484d7d629ab6e1c171034f4aeae9
Tommaso Brini <tommaso.brini@studio.unibo.it>
2025-12-10 15:24:17 +0100

    chore(validator): add PipeValidator for dto control

--- services/interactions/src/main.ts
+++ services/interactions/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
 
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.enableShutdownHooks();
 
   const port = process.env.INTERACTIONS_SERVICE_PORT ?? 9030;
