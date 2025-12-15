bf8fc95105a2802acaf7b87b5b0ae661046943f7
Fede802 <federico.bravetti2@studio.unibo.it>
2025-12-15 20:03:25 +0100

    fix(protocol): use https link in dev deployed environment

--- services/frontend/src/api/services/media.ts
+++ services/frontend/src/api/services/media.ts
@@ -1,8 +1,15 @@
 import type { MediaAPI, MediaGetResponse } from '../interfaces/media'
 
+const parseUrl = (url: string): string => {
+  if (url.startsWith('http://') && !import.meta.env.DEV) {
+    return url.replace('http://', 'https://')
+  }
+  return url
+}
+
 export const mediaApi: MediaAPI = {
   async get(url: string): Promise<MediaGetResponse> {
-    const response = await fetch(url)
+    const response = await fetch(parseUrl(url))
     const blob = await response.blob()
     const filename = url.split('/').pop() || `media-file`
     return { file: new File([blob], filename, { type: blob.type }) }
