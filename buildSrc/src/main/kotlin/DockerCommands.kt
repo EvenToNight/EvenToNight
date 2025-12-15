8a85511e83ff0e20521a6e44592681967108af75
Alice Alfonsi <alice.alfonsi2@studio.unibo.it>
2025-12-15 11:12:22 +0100

    chore(keycloak): set --project-name to keycloak-stack for Keycloak services

--- buildSrc/src/main/kotlin/DockerCommands.kt
+++ buildSrc/src/main/kotlin/DockerCommands.kt
@@ -2,9 +2,9 @@ object DockerCommands {
     const val TEARDOWN_TEST_ENVIRONMENT = "./scripts/composeDevEnvironment.sh --project-name eventonight-test-environment down -v --remove-orphans"
     const val TEARDOWN_DEV_ENVIRONMENT = "./scripts/composeDevEnvironment.sh --dev down -v --remove-orphans"
     const val TEARDOWN_APPLICATION_ENVIRONMENT = "./scripts/composeApplication.sh down --dev -v --remove-orphans"
-    const val TEARDOWN_KEYCLOAK = "./scripts/composeAll.sh -p ./infrastructure/authentication/ --dev down -v --remove-orphans"
+    const val TEARDOWN_KEYCLOAK = "./scripts/composeAll.sh -p ./infrastructure/authentication/ --dev down -v --remove-orphans --project-name keycloak-stack"
     const val SETUP_TEST_ENVIRONMENT = "./scripts/composeDevEnvironment.sh --project-name eventonight-test-environment up -d --force-recreate --wait"
     const val SETUP_DEV_ENVIRONMENT = "./scripts/composeDevEnvironment.sh --dev up -d --force-recreate --wait"
     const val SETUP_APPLICATION_ENVIRONMENT = "./scripts/composeApplication.sh --dev up -d --build --force-recreate --wait"
-    const val SETUP_KEYCLOAK = "./scripts/composeAll.sh -p ./infrastructure/authentication/ --dev up --force-recreate --build -d --wait"
+    const val SETUP_KEYCLOAK = "./scripts/composeAll.sh -p ./infrastructure/authentication/ --dev up --force-recreate --build -d --wait --project-name keycloak-stack"
 }
