170ec2033ddb64613d1b5f401702c880a9b699eb
Fede802 <federico.bravetti2@studio.unibo.it>
2025-12-15 12:50:44 +0100

    chore(Button): move outline button class inside Button

--- services/frontend/src/components/buttons/basicButtons/Button.vue
+++ services/frontend/src/components/buttons/basicButtons/Button.vue
@@ -62,3 +62,144 @@ withDefaults(defineProps<Props>(), {
   }
 }
 </style>
+
+<style lang="scss">
+.outline-btn-fix.q-btn.q-btn--outline {
+  color: #6f00ff !important;
+  border-color: #6f00ff !important;
+  background: transparent !important;
+
+  .q-btn__content,
+  .q-btn__content span,
+  .q-btn__content div,
+  .q-btn__content div span,
+  .q-btn__content .block,
+  span,
+  span.block,
+  span.q-ml-sm,
+  .q-icon,
+  i,
+  i.q-icon,
+  i.material-icons {
+    color: #6f00ff !important;
+  }
+
+  // Deep selectors for scoped components
+  :deep(.q-icon),
+  :deep(i),
+  :deep(i.material-icons),
+  :deep(span),
+  :deep(span.q-ml-sm),
+  :deep(div),
+  :deep(div span),
+  :deep(.row span) {
+    color: #6f00ff !important;
+  }
+
+  // Fix all interactive states
+  &.q-btn--active,
+  &:active,
+  &:hover,
+  &:focus,
+  &.q-hoverable:hover,
+  &.q-focusable:focus {
+    color: #6f00ff !important;
+    background: rgba(111, 0, 255, 0.1) !important;
+
+    .q-btn__content,
+    .q-btn__content span,
+    .q-btn__content div,
+    .q-btn__content div span,
+    .q-btn__content .block,
+    span,
+    span.block,
+    span.q-ml-sm,
+    .q-icon,
+    i,
+    i.q-icon,
+    i.material-icons {
+      color: #6f00ff !important;
+    }
+
+    :deep(.q-icon),
+    :deep(i),
+    :deep(i.material-icons),
+    :deep(span),
+    :deep(span.q-ml-sm),
+    :deep(div),
+    :deep(div span),
+    :deep(.row span) {
+      color: #6f00ff !important;
+    }
+  }
+}
+
+body.body--dark .outline-btn-fix.q-btn.q-btn--outline {
+  color: #bb86fc !important;
+  border-color: #bb86fc !important;
+  background: transparent !important;
+
+  .q-btn__content,
+  .q-btn__content span,
+  .q-btn__content div,
+  .q-btn__content div span,
+  .q-btn__content .block,
+  span,
+  span.block,
+  span.q-ml-sm,
+  .q-icon,
+  i,
+  i.q-icon,
+  i.material-icons {
+    color: #bb86fc !important;
+  }
+
+  :deep(.q-icon),
+  :deep(i),
+  :deep(i.material-icons),
+  :deep(span),
+  :deep(span.q-ml-sm),
+  :deep(div),
+  :deep(div span),
+  :deep(.row span) {
+    color: #bb86fc !important;
+  }
+
+  // Fix all interactive states in dark mode
+  &.q-btn--active,
+  &:active,
+  &:hover,
+  &:focus,
+  &.q-hoverable:hover,
+  &.q-focusable:focus {
+    color: #bb86fc !important;
+    background: rgba(187, 134, 252, 0.1) !important;
+
+    .q-btn__content,
+    .q-btn__content span,
+    .q-btn__content div,
+    .q-btn__content div span,
+    .q-btn__content .block,
+    span,
+    span.block,
+    span.q-ml-sm,
+    .q-icon,
+    i,
+    i.q-icon,
+    i.material-icons {
+      color: #bb86fc !important;
+    }
+
+    :deep(.q-icon),
+    :deep(i),
+    :deep(i.material-icons),
+    :deep(span),
+    :deep(span.q-ml-sm),
+    :deep(div),
+    :deep(div span),
+    :deep(.row span) {
+      color: #bb86fc !important;
+    }
+  }
+}
+</style>
