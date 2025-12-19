8cca1a5b17c002b81c9b375e62a5abb310a3d084
Tommaso Brini <tommaso.brini@studio.unibo.it>
2025-12-19 14:52:52 +0100

    chore(review): provide api to update and delete reviews

--- services/interactions/src/events/services/review.service.ts
+++ services/interactions/src/events/services/review.service.ts
@@ -8,6 +8,7 @@ import { Model } from 'mongoose';
 import { Review } from '../schemas/review.schema';
 import { CreateReviewDto } from '../dto/create-review.dto';
 import { MetadataService } from 'src/metadata/services/metadata.service';
+import { UpdateReviewDto } from '../dto/update-review.dto';
 
 @Injectable()
 export class ReviewService {
@@ -39,5 +40,31 @@ export class ReviewService {
       ...createReviewDto,
     });
     return review.save();
+  }
+
+  async deleteReview(eventId: string, userId: string): Promise<void> {
+    const result = await this.reviewModel.deleteOne({ eventId, userId });
+
+    if (result.deletedCount === 0) {
+      throw new NotFoundException('Review not found');
+    }
+  }
+
+  async updateReview(
+    eventId: string,
+    userId: string,
+    updateReviewDto: UpdateReviewDto,
+  ): Promise<Review> {
+    const review = await this.reviewModel.findOneAndUpdate(
+      { eventId, userId },
+      { rating: updateReviewDto.rating, comment: updateReviewDto.comment },
+      { new: true },
+    );
+
+    if (!review) {
+      throw new NotFoundException('Review not found');
+    }
+
+    return review;
   }
 }
