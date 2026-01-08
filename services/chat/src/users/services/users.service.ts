c9c57feb3b7a5ebaeda3130200b0fc623c47c270
Tommaso Brini <tommaso.brini@studio.unibo.it>
2026-01-08 14:47:59 +0100

    chore(users-insert): insert or update user info

---
+++ services/chat/src/users/services/users.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from '../schemas/user.schema';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<any>) {}
+
+  async upsertUser(data: {
+    userId: string;
+    name?: string;
+    avatar?: string;
+  }): Promise<User> {
+    return this.userModel.findOneAndUpdate({ userId: data.userId }, data, {
+      upsert: true,
+      new: true,
+    });
+  }
+}
