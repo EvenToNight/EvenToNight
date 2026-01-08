6996e9e8b851342f62cafaedcc4ac9a222710fe6
Tommaso Brini <tommaso.brini@studio.unibo.it>
2026-01-08 14:51:56 +0100

    feat(users-insert): provide handler for message to insert or update user info

---
+++ services/chat/src/users/controllers/users.controller.ts
@@ -0,0 +1,32 @@
+import { Controller } from '@nestjs/common';
+import {
+  MessagePattern,
+  Payload,
+  Ctx,
+  RmqContext,
+} from '@nestjs/microservices';
+import { UsersService } from '../services/users.service';
+
+@Controller()
+export class UserEventsConsumer {
+  constructor(private readonly usersService: UsersService) {}
+
+  @MessagePattern('user.created')
+  async handleUserCreated(@Payload() data: any, @Ctx() context: RmqContext) {
+    console.log('Received user.created event:', data);
+
+    try {
+      await this.usersService.upsertUser({
+        userId: data.userId,
+        name: data.name,
+        avatar: data.avatar,
+      });
+
+      const channel = context.getChannelRef();
+      const originalMsg = context.getMessage();
+      channel.ack(originalMsg);
+    } catch (error) {
+      console.error('❌ Error caching user:', error);
+    }
+  }
+}
