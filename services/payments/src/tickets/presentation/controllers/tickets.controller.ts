394196bea570dfd84f089f1f4ca50cc252ca1bd6
Fede802 <federico.bravetti2@studio.unibo.it>
2026-01-16 11:54:45 +0100

    refactor(controllers): exctract users controller from tickets controller

--- services/payments/src/tickets/presentation/controllers/tickets.controller.ts
+++ services/payments/src/tickets/presentation/controllers/tickets.controller.ts
@@ -1,43 +1,16 @@
-import {
-  Controller,
-  Get,
-  Param,
-  Res,
-  NotFoundException,
-  Query,
-} from '@nestjs/common';
+import { Controller, Get, Param, Res, NotFoundException } from '@nestjs/common';
 import type { Response } from 'express';
 import { PdfService } from '../../application/services/pdf.service';
 import { TicketService } from 'src/tickets/application/services/ticket.service';
-import { PaginatedQueryDto } from 'src/commons/application/dto/paginated-query.dto';
-import { Pagination } from 'src/tickets/utils/pagination.utils';
-import { Ticket } from 'src/tickets/domain/aggregates/ticket.aggregate';
-import { PaginatedResponseDto } from 'src/commons/application/dto/paginated-response.dto';
 
-@Controller()
+@Controller('tickets')
 export class TicketsController {
   constructor(
     private readonly pdfService: PdfService,
     private readonly ticketService: TicketService,
   ) {}
 
-  /**
-   * GET /users/:userId/tickets
-   * Returns all tickets for the specified user.
-   */
-  @Get('users/:userId/tickets/')
-  async getUserTickets(
-    @Param('userId') userId: string,
-    @Query() query: PaginatedQueryDto,
-  ): Promise<PaginatedResponseDto<Ticket>> {
-    const tickets = await this.ticketService.findByUserId(
-      userId,
-      Pagination.parse(query.limit, query.offset),
-    );
-    return tickets;
-  }
-
-  @Get('tickets/:ticketId')
+  @Get(':ticketId')
   async getUserTicket(@Param('ticketId') ticketId: string) {
     const ticket = await this.ticketService.findById(ticketId);
     if (!ticket) {
@@ -50,7 +23,7 @@ export class TicketsController {
    * GET /tickets/:ticketId/pdf
    * Returns a PDF for the specified ticket.
    */
-  @Get('tickets/:ticketId/pdf')
+  @Get(':ticketId/pdf')
   async getTicketPdf(
     @Param('ticketId') ticketId: string,
     @Res() res: Response,
---
+++ services/payments/src/tickets/presentation/controllers/users.controller.ts
@@ -0,0 +1,27 @@
+import { Controller, Get, Param, Query } from '@nestjs/common';
+import { TicketService } from 'src/tickets/application/services/ticket.service';
+import { PaginatedQueryDto } from 'src/commons/application/dto/paginated-query.dto';
+import { Pagination } from 'src/tickets/utils/pagination.utils';
+import { Ticket } from 'src/tickets/domain/aggregates/ticket.aggregate';
+import { PaginatedResponseDto } from 'src/commons/application/dto/paginated-response.dto';
+
+@Controller('users/:userId')
+export class TicketsController {
+  constructor(private readonly ticketService: TicketService) {}
+
+  /**
+   * GET /users/:userId/tickets
+   * Returns all tickets for the specified user.
+   */
+  @Get('tickets/')
+  async getUserTickets(
+    @Param('userId') userId: string,
+    @Query() query: PaginatedQueryDto,
+  ): Promise<PaginatedResponseDto<Ticket>> {
+    const tickets = await this.ticketService.findByUserId(
+      userId,
+      Pagination.parse(query.limit, query.offset),
+    );
+    return tickets;
+  }
+}
