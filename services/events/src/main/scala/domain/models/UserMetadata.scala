845ad358aa487cb865c86bd563685134ee5b3f98
Tommaso Brini <tommaso.brini@studio.unibo.it>
2026-01-22 14:54:55 +0100

    chore(user-metadata): implement conversion to document for user metadata

--- services/events/src/main/scala/domain/models/UserMetadata.scala
+++ services/events/src/main/scala/domain/models/UserMetadata.scala
@@ -4,3 +4,18 @@ case class UserMetadata(
     id: String,
     role: String
 )
+
+object UserMetadata:
+  import org.bson.Document
+
+  extension (user: UserMetadata)
+    def toDocument: Document =
+      val doc = new Document().append("id", user.id)
+      doc.append("role", user.role)
+      doc
+
+  def fromDocument(doc: Document): UserMetadata =
+    UserMetadata(
+      id = doc.getString("id"),
+      role = doc.getString("role")
+    )
