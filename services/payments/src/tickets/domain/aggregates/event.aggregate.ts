25a9ebd79d659d33eebc3e95de6d9be82090b725
Fede802 <federico.bravetti2@studio.unibo.it>
2026-01-23 10:32:49 +0100

    chore(events): update event aggregate addig date and status fields

--- services/payments/src/tickets/domain/aggregates/event.aggregate.ts
+++ services/payments/src/tickets/domain/aggregates/event.aggregate.ts
@@ -1,14 +1,24 @@
 import { UserId } from '../value-objects/user-id.vo';
 import { EventId } from '../value-objects/event-id.vo';
+import { EventStatus } from '../value-objects/event-status.vo';
+
+export interface EventCreateParams {
+  id: EventId;
+  creatorId: UserId;
+  date: Date;
+  status: EventStatus;
+}
 
 export class Event {
   private constructor(
     private readonly id: EventId,
     private creatorId: UserId,
+    private date: Date,
+    private status: EventStatus,
   ) {}
 
-  static create(id: EventId, creatorId: UserId): Event {
-    return new Event(id, creatorId);
+  static create(params: EventCreateParams): Event {
+    return new Event(params.id, params.creatorId, params.date, params.status);
   }
 
   // Getters
@@ -18,5 +28,13 @@ export class Event {
 
   getCreatorId(): UserId {
     return this.creatorId;
+  }
+
+  getDate(): Date {
+    return this.date;
+  }
+
+  getStatus(): EventStatus {
+    return this.status;
   }
 }
