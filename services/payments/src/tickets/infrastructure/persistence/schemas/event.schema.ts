241a002b4c78efc0983dd8c5efba5d6ddfedd2b7
Fede802 <federico.bravetti2@studio.unibo.it>
2026-01-23 10:34:24 +0100

    chore(events): update event schema and mapper

--- services/payments/src/tickets/infrastructure/persistence/schemas/event.schema.ts
+++ services/payments/src/tickets/infrastructure/persistence/schemas/event.schema.ts
@@ -13,6 +13,12 @@ export class EventDocument {
 
   @Prop({ required: true })
   creatorId: string;
+
+  @Prop({ required: true })
+  date: Date;
+
+  @Prop({ required: true })
+  status: string;
 }
 
 export type EventDocumentType = HydratedDocument<EventDocument>;
