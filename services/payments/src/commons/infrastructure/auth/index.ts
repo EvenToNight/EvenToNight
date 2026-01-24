3904f56219ec03769b7bc1b09b2ada8845386066
Fede802 <federico.bravetti2@studio.unibo.it>
2026-01-24 13:47:24 +0100

    chore(guards): add optional auth guard

--- services/payments/src/commons/infrastructure/auth/index.ts
+++ services/payments/src/commons/infrastructure/auth/index.ts
@@ -1,5 +1,6 @@
 export { JwtStrategy } from './jwt.strategy';
 export type { AuthUser, JwtPayload } from './jwt.strategy';
 export { JwtAuthGuard } from './jwt-auth.guard';
+export { OptionalJwtAuthGuard } from './optional-jwt-auth.guard';
 export { CurrentUser } from './current-user.decorator';
 export { AuthModule } from './auth.module';
