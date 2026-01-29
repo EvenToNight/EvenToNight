5e3d487b3fcb2710bc21717f558fe5da48f37e7b
Tommaso Brini <tommaso.brini@studio.unibo.it>
2026-01-29 11:24:16 +0100

    chore(notification): implement external event mapper

---
+++ services/notifications/src/notifications/application/mapper/external-event.mapper.ts
@@ -0,0 +1,28 @@
+import { CreateNotificationFromEventCommand } from "../commands/create-notification-from-event.command";
+
+export class ExternalEventMapper {
+  static mapToCommand(
+    routingKey: string,
+    payload: any,
+  ): CreateNotificationFromEventCommand | null {
+    switch (routingKey) {
+      // TODO: handle all routing keys
+      case "interaction.like":
+        return this.mapInteractionLike(payload);
+      default:
+        console.warn(`No mapping found for routing key: ${routingKey}`);
+        return null;
+    }
+  }
+
+  private static mapInteractionLike(
+    payload: any,
+  ): CreateNotificationFromEventCommand {
+    return CreateNotificationFromEventCommand.create({
+      recipientUserId: payload.targetOwnerId,
+      type: "like",
+      title: "New Like",
+      message: `Your post has a new like`,
+    });
+  }
+}
