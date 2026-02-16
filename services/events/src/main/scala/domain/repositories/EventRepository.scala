73a59ebfc1a0f921ad80be6c146bedddb7d8e665
Tommaso Brini <tommaso.brini@studio.unibo.it>
2026-02-16 11:26:59 +0100

    refactor(events): move interface repository in domain

---
+++ services/events/src/main/scala/domain/repositories/EventRepository.scala
@@ -0,0 +1,39 @@
+package domain.repositories
+
+import domain.aggregates.Event
+import domain.models.EventStatus
+import domain.valueobjects.{EventId, OrganizationId}
+
+import java.time.LocalDateTime
+
+trait EventRepository:
+
+  def save(event: Event): Either[String, Unit]
+
+  def save(event: Event, ctx: TransactionContext): Either[String, Unit]
+
+  def findById(eventId: EventId): Option[Event]
+
+  def delete(eventId: EventId): Either[String, Unit]
+
+  def findAllPublished(): Either[String, List[Event]]
+
+  def findByCreator(creatorId: OrganizationId): List[Event]
+
+  def findByFilters(
+      limit: Option[Int] = None,
+      offset: Option[Int] = None,
+      status: Option[List[EventStatus]] = None,
+      title: Option[String] = None,
+      tags: Option[List[String]] = None,
+      startDate: Option[LocalDateTime] = None,
+      endDate: Option[LocalDateTime] = None,
+      organizationId: Option[String] = None,
+      city: Option[String] = None,
+      locationName: Option[String] = None,
+      sortBy: Option[String] = None,
+      sortOrder: Option[String] = None,
+      query: Option[String] = None,
+      near: Option[(Double, Double)] = None,
+      priceRange: Option[(Double, Double)] = None
+  ): Either[String, (List[Event], Boolean)]
