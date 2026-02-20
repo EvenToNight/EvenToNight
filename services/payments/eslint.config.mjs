bd58595b80b4dba5c7562eb888380145bd135819
Fede802 <federico.bravetti2@studio.unibo.it>
2026-02-20 16:24:35 +0100

    test(paymentss): update jest configuration

--- services/payments/eslint.config.mjs
+++ services/payments/eslint.config.mjs
@@ -19,7 +19,9 @@ export default tseslint.config(
       },
       sourceType: 'commonjs',
       parserOptions: {
-        projectService: true,
+        projectService: {
+          allowDefaultProject: ['*.config.js'],
+        },
         tsconfigRootDir: import.meta.dirname,
       },
     },
---
+++ services/payments/jest.config.js
@@ -0,0 +1,16 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  projects: [
+    '<rootDir>/test/unit/jest-unit.json',
+    '<rootDir>/test/integration/jest-integration.json',
+    '<rootDir>/test/e2e/jest-e2e.json',
+  ],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/*.module.ts',
+    '!<rootDir>/src/main.ts',
+    '!<rootDir>/src/**/*.d.ts',
+  ],
+  coverageDirectory: '<rootDir>/coverage',
+  testTimeout: 30000,
+};
